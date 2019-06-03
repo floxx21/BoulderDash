2eee0991bfdee380b82a7e38af6472a162aa17bf
novaf <novaf@LAPTOP-KTDASSRN>
2019-06-03 14:40:27 +0200

    Test Model 1

---
+++ model/src/test/java/model/DAOEntityTest.java
@@ -0,0 +1,64 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DAOEntityTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDAOEntity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetConnection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindString() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ model/src/test/java/model/DBPropertiesTest.java
@@ -0,0 +1,49 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DBPropertiesTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDBProperties() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUrl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+}
